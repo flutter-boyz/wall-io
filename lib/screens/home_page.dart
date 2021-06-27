f373da7f63502ed13084afe513c9700f9747ebb6
Aitor Trujillo <zenji@MacBook-Air-de-Aitor.local>
2021-06-27 16:05:37 +0200

    async call & load image

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import '../models/image_model.dart';
 import '../util/api_provider.dart';
 
@@ -10,12 +11,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Hits> hits;
+  List<Hits>? hits;
+  Hits? hit;
 
   _loadImages() async {
     var imageModel = await ApiProvider().getImages(5);
     setState(() {
       hits = imageModel.hits;
+      hit = imageModel.hits![0];
     });
   }
 
@@ -32,11 +35,11 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Wall-io'),
       ),
       body: Center(
-        child: hits.length > 0
-            ? Image(
-                image: hits.first.pageUrl,
-              )
-            : null,
+        child: CachedNetworkImage(
+          imageUrl: hit!.webformatURL ??
+              "https://pixabay.com/get/gc5b5182455b4aabbb5fcc7fccd7faf84d112021938a7c92e0c14bbae3cbedb4094e75fc453bc6bc19abaddce57a0c29e5a93e3d3fbdfa250bc540704040bdedf_640.jpg",
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
